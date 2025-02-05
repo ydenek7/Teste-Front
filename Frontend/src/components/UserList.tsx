4cae5a88adb498fd82b7555f82a9abf04084f4a3
Kenedy <gabriel.kndyy@gmail.com>
2025-02-05 18:43:51 -0300

    Update UserList.tsx

--- Frontend/src/components/UserList.tsx
+++ Frontend/src/components/UserList.tsx
@@ -1,18 +1,50 @@
-import React from "react";
-import { useUserContext } from "../context/UserContext.tsx"; 
+import React, { useState } from "react";
+import useUserContext from '../context/UserContext.tsx';
 
 
-const UserList : React.FC = () => {
+
+const UserList: React.FC = () => {
     const { users, createUser, deleteUser } = useUserContext();
 
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleAddUser = () => {
+        if (name && email) {
+            createUser(name, email);
+            setName('');
+            setEmail('');
+        } else {
+            alert("preencha todos os campos")
+        }
+
+    }
+
     return (
+
+
         <div style={{ padding: "20px", maxWidth: "600px", margin: "auto" }}>
+
+
+            <input type="text"
+                placeholder="nome"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                style={{ marginBottom: '10px', padding: '5px', width: '100%' }} />
+
+            <input type="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ marginBottom: '10px', padding: '5px', width: '100%' }} />
+
+
             <h2>Lista</h2>
+
+
             <button
-                onClick={() => createUser("Novo usuário", "novo@email")}
-                style={{ marginBottom: "10px", padding: "5px", cursor: "pointer" }}
-
-
+                onClick={handleAddUser}
+                style={{ marginBottom: '10px', padding: '5px', cursor: 'pointer' }}
             >Adicionar</button>
 
             <ul>
